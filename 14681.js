4cb98d2260a7aedb4e6dddadf55375941c6eb01b
liberoLab <libero221.lab@gmail.com>
2022-01-26 20:04:54 +0900

    문제 해결

---
+++ 14681.js
@@ -0,0 +1,14 @@
+let x = 12
+let y = 5
+
+if (x > 0 && y > 0) {
+    console.log("Quadrant 1")
+} else if (x < 0 && y > 0) {
+    console.log("Quadrant 2")
+} else if (x < 0 && y < 0) {
+    console.log("Quadrant 3")
+} else if (x > 0 && y < 0) {
+    console.log("Quadrant 4")
+} else {
+    console.log("0")
+}
