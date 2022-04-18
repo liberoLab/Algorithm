9b7ac46ad19555c4ac9b97cef31a71b045459ec2
liberoLab <libero221.lab@gmail.com>
2022-04-19 02:26:02 +0900

    정리

--- programmers/12935.js
+++ programmers/12935.js
@@ -2,3 +2,15 @@
 제일 작은 수 제거하기
 https://programmers.co.kr/learn/courses/30/lessons/12935?language=javascript
 */
+
+function solution(arr) {
+  arr.sort((a, b) => b - a);
+  if (arr.length != 1) {
+    arr.pop();
+  } else {
+    arr[0] = -1;
+  }
+  return arr;
+}
+
+console.log(solution([4, 3, 2, 1]));
